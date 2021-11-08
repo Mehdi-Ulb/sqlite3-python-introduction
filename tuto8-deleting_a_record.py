1e1cadcf647d3a2e5b9cff59cef380cb4926261d
vdirken <A4rXQcBxKXbQHxvr2fsM>
2021-11-08 23:16:29 +0100

    Delete

--- tuto8-deleting_a_record.py
+++ tuto8-deleting_a_record.py
@@ -42,7 +42,7 @@ def read_data():
 
 #insert_record(8, 'Romain', 'Hardware', 5)
 # Following Python function shows
-# how to use UPDATE statement to update any record
+# how to use DELETE statement to delete any record
 def delete_record(id):
     dbase.execute(''' DELETE from employee_records WHERE ID =''' + str(id))
     dbase.commit()
